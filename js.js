98124f9f8cefe2c05519b90ab031e4c6ee3cf362
DzsozefByte <ssjocijr@gmail.com>
2025-01-07 12:23:21 +0100

    VEGSO

--- js.js
+++ js.js
@@ -4,29 +4,34 @@ let szamlalo = 0;
 
 kartyanevek.forEach(kartyanev => {
     kartyanev.addEventListener('dragstart', (event) => {
-        event.target.id = "kartyanev_" + szamlalo; // ID beállítása
-        event.dataTransfer.setData('text/plain', event.target.id); // Az id mentése
+        event.target.id = "kartyanev_" + szamlalo; 
+        event.dataTransfer.setData('text/plain', event.target.id); 
         szamlalo++;
     });
 });
 
 Array.from(hely).forEach(element => {
+    let vizsg = false;
     element.addEventListener('drop', (event) => {
-        event.preventDefault(); // Megakadályozzuk az alapértelmezett viselkedést
-        const id = event.dataTransfer.getData('text/plain'); // Az elvitt kártya ID-ja
-        const fogottkartya = document.getElementById(id); // Kártya elem lekérése
-        element.appendChild(fogottkartya); // A kártya hozzáadása a célhelyhez
-        element.style.backgroundColor = ""; // A háttérszín visszaállítása
-        fogottkartya.style.margin="0px";
+        event.preventDefault(); 
+        const id = event.dataTransfer.getData('text/plain'); 
+        const fogottkartya = document.getElementById(id);
+        if(vizsg==false){
+            element.appendChild(fogottkartya); 
+            element.style.backgroundColor = ""; 
+            fogottkartya.style.margin="0px";
+            element.style.opacity= "100%";
+            vizsg=true;
+        } 
         
     });
 
     element.addEventListener('dragover', (event) => {
-        event.preventDefault(); // Engedélyezzük az elem áthúzását
-        element.style.backgroundColor = "#DA291C"; // Átmeneti színváltoztatás, jelezve, hogy ide húzható
+        event.preventDefault(); 
+        element.style.backgroundColor = "#DA291C"; 
     });
 
     element.addEventListener('dragleave', () => {
-        element.style.backgroundColor = ""; // Töröljük a háttérszínt, ha elhagyjuk a területet
+        element.style.backgroundColor = ""; 
     });
 });
